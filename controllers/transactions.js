08ed2ab9c6842759f69eaef65d053a2ba85357ef
Hallcoder <hallcoder25@gmail.com>
2023-08-19 07:00:59 +0200

    idk

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -14,7 +14,6 @@ export const getTransactions = async (req, res) => {
 export const postTransactions = async (req, res) => {
   const { products } = req.body;
   const  newProductsInfo = [];
-  var transactionProducts=[]
   console.log(products)
   let totalAmount = 0;
   try {
@@ -41,21 +40,15 @@ export const postTransactions = async (req, res) => {
          error.status = 403;
          throw new Error(error);
         }else{
-          productFound.supply -= product.unitsTaken;
+          productFound.supply -= product.unitsTaken;x
         }
         productFound.save();
-        transactionProducts.push({
-          ...productFound,
-          id:productFound._id
-        })
         newProductsInfo.push(productFound);
       }
     }
 
-
     const sale = await Transaction.create({
-      ...req.body,
-      products:transactionProducts,
+      products,
       totalAmount,
     });
     res.status(201).json({transaction:sale,newProductsInfo});
